a23696c49e2e9ff4a058952ec1beebbe3d3d449e
Kim Ewing <pekar.kimberly@gmail.com>
2024-04-04 16:49:49 -0600

    created sides, mains, and desserts variables

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ var mains = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai 
 
 var desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
 
+
